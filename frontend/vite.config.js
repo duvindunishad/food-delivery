f6517c1e30a82d92a7eb6214b4a6edf4b7807873
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-07-05 15:00:22 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '',
   plugins: [react()],
   build: {
     outDir: 'build',  // Ensures the output directory is 'build'
