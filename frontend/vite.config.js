bda8f2348fa629b77f093130a503a186d1233cbc
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-07-05 14:10:53 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: '/food-delivery/',
+  base: '/food-delivery/',
   plugins: [react()],
   build: {
     outDir: 'build',  // Ensures the output directory is 'build'
